2384ba7375e5245a2efba3e607470f96a77d06fd
link2x <link2x@users.noreply.github.com>
2022-10-23 00:43:41 -0400

    v1

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,42 @@ import logo from '../public/logo.png'
 export default function Home() {
 
 	const textOptions = [
+		// Intro Text / Artist Name
 		'self consciously aware',
+		
+		// Randoms
 		'i\'m glad you\'re here',
-		'thank you for being here',
 		'some days, i wish i could take it all back',
+
+		// I Hope You Understand / Selfish
+		'there\'s nothing wrong with me, and yet everything',
+		'i\'m crying all the time for more than you understand',
+
+		// It's Not Getting Better
+		'the words you spoke don\'t resonate inside me',
+		'in the same way that I always was',
+
+		// Waste Your Time
+		'it\'s okay if you want me to let you go',
+		'don\'t i really waste your time?',
+
+		// MOS
+		'i\'ll start with a new plan. start once again',
+		'i never meant for these nightmares to come true',
+
+		// Falter
+		'i can\'t stand for this. i don\'t trust you anymore',
+		'you said that we\'ll stretch too thin',
+
+		// To Rest
+		'are you human? do you care at all?',
+		'i will go quietly',
+
+		// Teasers?
+		'words of ash and paper will melt into the sun',
+		'sleep tonight - you morning riser, you',
+
+		// Pride
 		'trans rights = human rights'
 	]
 
@@ -42,6 +74,12 @@ export default function Home() {
 			</div>
 			<div className='typewriter'>
 				<p id={typewriterActive ? 'write' : null} onClick={resetText} >{typewriterText}</p>
+			</div>
+			<div className='footer'>
+				<span>
+					you're here early. more soon.<br />
+					2022 SCA
+				</span>
 			</div>
 		</div>
 	)
